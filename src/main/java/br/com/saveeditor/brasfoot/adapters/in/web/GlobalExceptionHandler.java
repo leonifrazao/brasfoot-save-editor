24a99c37614fb37feccca17190ef68fa97400428
leoni frazão <leoni.frazao.oliveira@gmail.com>
2026-04-05 18:40:25 -0300

    feat(260405-pv4-01): increase multipart upload limits to 500MB

--- src/main/java/br/com/saveeditor/brasfoot/adapters/in/web/GlobalExceptionHandler.java
+++ src/main/java/br/com/saveeditor/brasfoot/adapters/in/web/GlobalExceptionHandler.java
@@ -4,9 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ProblemDetail handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.PAYLOAD_TOO_LARGE, "File size exceeds the 500MB limit.");
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ProblemDetail handleIllegalArgumentException(IllegalArgumentException ex) {
