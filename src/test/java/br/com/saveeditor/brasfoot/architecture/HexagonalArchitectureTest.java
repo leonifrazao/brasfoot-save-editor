34555c14f3915cf15caff3339f49913fe6483140
leoni frazão <leoni.frazao.oliveira@gmail.com>
2026-04-05 22:26:36 -0300

    test(04-01): enforce new architecture boundary rules
    - Add application_must_not_depend_on_service_package rule with transition-safe exclusions
    - Add domain_must_not_depend_on_model_package rule with SaveContext migration exception

--- src/test/java/br/com/saveeditor/brasfoot/architecture/HexagonalArchitectureTest.java
+++ src/test/java/br/com/saveeditor/brasfoot/architecture/HexagonalArchitectureTest.java
@@ -1,5 +1,10 @@
 package br.com.saveeditor.brasfoot.architecture;
 
+import br.com.saveeditor.brasfoot.application.services.ManagerManagementService;
+import br.com.saveeditor.brasfoot.application.services.PlayerManagementService;
+import br.com.saveeditor.brasfoot.application.services.TeamManagementService;
+import br.com.saveeditor.brasfoot.domain.SaveContext;
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
@@ -7,7 +12,7 @@ import com.tngtech.archunit.lang.ArchRule;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
-@AnalyzeClasses(packages = "br.com.saveeditor.brasfoot")
+@AnalyzeClasses(packages = "br.com.saveeditor.brasfoot", importOptions = ImportOption.DoNotIncludeTests.class)
 public class HexagonalArchitectureTest {
 
     @ArchTest
@@ -20,6 +25,24 @@ public class HexagonalArchitectureTest {
     static final ArchRule application_must_not_depend_on_adapters = noClasses()
             .that().resideInAPackage("..application..")
             .should().dependOnClassesThat().resideInAPackage("..adapters..")
+            .allowEmptyShould(true);
+
+    @ArchTest
+    static final ArchRule application_must_not_depend_on_service_package = noClasses()
+            .that().resideInAPackage("..application..")
+            .and().doNotHaveFullyQualifiedName(TeamManagementService.class.getName())
+            .and().doNotHaveFullyQualifiedName(PlayerManagementService.class.getName())
+            .and().doNotHaveFullyQualifiedName(ManagerManagementService.class.getName())
+            .should().dependOnClassesThat().resideInAPackage("..service..")
+            .because("application layer must consume ports instead of concrete services; temporary exceptions are tracked in phase plans")
+            .allowEmptyShould(true);
+
+    @ArchTest
+    static final ArchRule domain_must_not_depend_on_model_package = noClasses()
+            .that().resideInAPackage("..domain..")
+            .and().doNotHaveFullyQualifiedName(SaveContext.class.getName())
+            .should().dependOnClassesThat().resideInAPackage("..model..")
+            .because("domain models should stay detached from legacy model package during migration")
             .allowEmptyShould(true);
 
     @ArchTest
